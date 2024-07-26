baa71add8deee707628c111753783b0679f5652f
bruno.merez <bruno.merez@flashcourier.com.br>
2024-07-26 00:09:15 -0300

    Corrigindo erro .env

--- src/env.ts
+++ src/env.ts
@@ -4,7 +4,7 @@ export default function setupEnv(): boolean {
   try {
     const isProd = process.env.NODE_ENV === "production";
     dotEnv.config({
-      path: isProd ? "./env" : "./.env.dev",
+      path: isProd ? "./.env" : "./.env.dev",
     });
 
     console.debug("Iniciando bot em ambiente de " + process.env.NODE_ENV);
